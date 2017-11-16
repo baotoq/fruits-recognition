0f2e16a8298477b44b2d2d7b807ad86509c98988
bao2703 <bao2703@gmail.com>
2017-11-17 01:47:34 +0700

    Up

--- detect.py
+++ detect.py
@@ -7,21 +7,23 @@ cascade = cv2.CascadeClassifier("cascade/cucumber_cascade.xml")
 def detect(img):
 	img = cv2.resize(img, (500, 500))
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	fruit = cascade.detectMultiScale(gray, 1.3, 5)
+	fruit = cascade.detectMultiScale(gray, 1.02, 10)
 	for (x, y, w, h) in fruit:
 			cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
 
 	return img
-
 def real_time():
-    cap = cv2.VideoCapture('dataset/cucumber/videos/1.mp4')
+    i = 0
+    cap = cv2.VideoCapture('dataset/cucumber/videos/3.mp4')
     while True:
         success, frame = cap.read()
         if success:
             img = detect(frame)
-            cv2.imshow('img', img)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            #cv2.imshow('img', img)
+            #if cv2.waitKey(1) & 0xFF == ord('q'):
+                #break
+            i+=1
+            cv2.imwrite('output/{}.jpg'.format(i), img)
         else:
             print('fail')
             break
